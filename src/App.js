3eac6e0fb93613894b268e114ab5bf2043e26f23
jagrutimetaliya <jagrutijmetaliya.me@gmail.com>
2021-02-01 23:00:18 +0530

    add fragment component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Fragment from './components/Fragment'
+function App() {
+  return (
+    <div className="App">
+       <Fragment/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Fragment.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Fragment() {
+    return (
+        <React.Fragment>
+            <h1>Fragemnt Demo</h1>
+            <p>This describe the Fragemnt Demo Component</p>
+        </React.Fragment>
+    )
+}
+
+export default Fragment
