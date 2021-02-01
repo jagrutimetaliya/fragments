6b8da7c2bfbfdc6094406a8edbf29aaef6c2a742
jagrutimetaliya <jagrutijmetaliya.me@gmail.com>
2021-02-01 23:13:17 +0530

    React Fragment components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Fragment from './components/Fragment'
+import Table from "./components/Table";
 function App() {
   return (
     <div className="App">
        <Fragment/>
+       <Table/>
     </div>
   );
 }
